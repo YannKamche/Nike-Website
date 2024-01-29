faa6585b2cd2d6875726d8d62994efa5672dd589
Yann Kamche <yannkamche@gmail.com>
2024-01-29 13:39:43 +0100

    Added a dynamically rendered year

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,6 +2,8 @@ import { copyrightSign } from "../assets/icons"
 import {footerLogo} from "../assets/images"
 import {footerLinks, socialMedia} from "../constants"
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer className='max-container'>
       <div className='flex justify-between items-start gap-20 flex-wrap max-lg:flex-col'>
@@ -22,7 +24,7 @@ const Footer = () => {
           <div className='flex items-center gap-5 mt-8'>
             {socialMedia.map((icon) => (
               <div
-                className='flex justify-center items-center w-12 h-12 bg-white rounded-full'
+                className='flex justify-center items-center w-12 h-12 bg-white rounded-full hover:bg-coral-red cursor-pointer transition-colors ease-in-out duration-300'
                 key={icon.alt}
               >
                 <img src={icon.src} alt={icon.alt} width={24} height={24} />
@@ -61,7 +63,7 @@ const Footer = () => {
             height={20}
             className='rounded-full m-0'
           />
-          <p>Copyright. All rights reserved.</p>
+          <p>Copyright. All rights reserved. {year}</p>
         </div>
         <p className='font-montserrat cursor-pointer'>Terms & Conditions</p>
       </div>
